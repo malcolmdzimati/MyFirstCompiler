fddc0f6741aae290e6e6dcd913e33088a28a15c5
malcolmdzimati <malcolmdzimati@gmail.com>
2022-05-15 01:08:02 +0200

    Added Code comments and added code to read file

--- Source Code/Main.java
+++ Source Code/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner
 
 
 public class Main{
+    //Function to Scanner Size of File
     public int static readFileSize(String filename){
         int size = 0;
         try{
@@ -13,11 +14,29 @@ public class Main{
             while(myReader.hasNextLine()){
                 size++;
             }
+            myReader.close();
         } catch (FileNotFoundException e){
             return -1;
         }
-        myReader.close();
         return size;
+    }
+
+    //Function to read code in file and put it into  a string array(2d char array)
+    public String[] static readFile(String filename){
+        int size = readFileSize(filename);
+        String code[size] = new String[];
+
+        try{
+            File myFile = new File(filename);
+            Scanner myReader = new Scanner(myFile);
+
+            for(int i = 0; i < Size; i++){
+                code[i] = myReader.nextLine();
+            }
+            myReader.close();
+        } catch (FileNotFoundException e){
+            return null;
+        }
     }
 
     public static void main(String[] args) {
