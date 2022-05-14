e10856052d8558f4fb1aae33afef5ed2869f1e09
malcolmdzimati <malcolmdzimati@gmail.com>
2022-05-15 01:28:25 +0200

    Able to read file and store in a string array.

--- Source Code/Main.java
+++ Source Code/Main.java
@@ -1,17 +1,18 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner
+import java.util.Scanner;
 
 
 public class Main{
     //Function to Scanner Size of File
-    public int static readFileSize(String filename){
+    public static int readFileSize(String filename){
         int size = 0;
         try{
             File myFile = new File(filename);
             Scanner myReader = new Scanner(myFile);
 
             while(myReader.hasNextLine()){
+                String line = myReader.nextLine();
                 size++;
             }
             myReader.close();
@@ -22,26 +23,27 @@ public class Main{
     }
 
     //Function to read code in file and put it into  a string array(2d char array)
-    public String[] static readFile(String filename){
-        int size = readFileSize(filename);
-        String code[size] = new String[];
+    public static String[] readFile(String filename, int fileSize){
+        int size = fileSize;
+        String[] code = new String[size];
 
         try{
             File myFile = new File(filename);
             Scanner myReader = new Scanner(myFile);
 
-            for(int i = 0; i < Size; i++){
+            for(int i = 0; i < size; i++){
                 code[i] = myReader.nextLine();
             }
             myReader.close();
         } catch (FileNotFoundException e){
             return null;
         }
+        return code;
     }
 
     //Function to promt user for Filename and returns it as a string
-    public String static readFileName(){
-        System.out.printl("Please Enter Name of File in the same directory as jar file or enter directoty to filename, with the filename.");
+    public static String readFileName(){
+        System.out.println("Please Enter Name of File in the same directory as jar file or enter directoty to filename, with the filename.");
         System.out.print("Enter The Name of The File: ");
 
         Scanner read = new Scanner(System.in);
@@ -50,7 +52,15 @@ public class Main{
         read.close();
         return filename;
     }
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        String fileName = readFileName();
+        int fileSize = readFileSize(fileName);
+        String[] fileContent = readFile(fileName, fileSize);
+
+        //Test Code to make Sure read in line correctly: Passed Test Reads in line properly
+        /*for(String line : fileContent){
+            System.out.println(line.length());
+        }*/
     }
 }
